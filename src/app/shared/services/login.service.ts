5dbcad6d7194ddf7acc33aef431dbec826aa1ce8
Harsh Pancholi <harshpancholi8@gmail.com>
2019-09-04 11:14:43 +0530

    added service to manage login, signup and logout

---
+++ src/app/shared/services/login.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BASE_URL } from 'src/app/app.constants';
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  login(payload) {
+    return this.http.post(`${BASE_URL}/login`, payload);
+  }
+
+  signUp(payload) {
+    return this.http.post(`${BASE_URL}/register`, payload);
+  }
+
+  logout(payload) {
+    return this.http.post(`${BASE_URL}/logout`, payload);
+  }
+}
